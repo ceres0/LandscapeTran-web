c7f061c0610d2c6aef4af547593b16bd7fa1934b
Ceres <guyihao0201@gmail.com>
2023-03-23 00:00:40 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { defineComponent } from 'vue'
+import Carousel from '@/components/Carousel.vue';
+</script>
+
+<template>
+  <main>
+    <n-loading-bar-provider>
+      <n-message-provider>
+        <n-notification-provider>
+          <n-dialog-provider>
+            <Carousel />
+          </n-dialog-provider>
+        </n-notification-provider>
+      </n-message-provider>
+    </n-loading-bar-provider>
+  </main>
+</template>
